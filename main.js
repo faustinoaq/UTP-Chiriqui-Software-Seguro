9286e8e6ac44269037c40607d7a05275c1f39327
Faustino Aguilar <faustinoaq@gmail.com>
2022-04-21 17:38:41 -0500

    Updating code

---
+++ main.js
@@ -0,0 +1,8 @@
+(function () {
+  var id = document.querySelector("#app")
+
+  id.insertAdjacentHTML("beforeend", `
+    <h1>¡Hola UTP Chiriqui!</h1>
+    <p>Desarrollando software para todos.</p>
+  `)
+})()
